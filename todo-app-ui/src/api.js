98154d1204c7dceab6f506669b1c717f3515aecd
“sunil” <sunilsahu0123@gmail.com>
2024-12-16 23:09:40 +0530

    backed url changed in ui

--- todo-app-ui/src/api.js
+++ todo-app-ui/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080/api/todos";
+const API_URL = "http://my-local-cluster/api/todos";
 
 export const getTodos = () => axios.get(API_URL);
 export const getColor = () => axios.get(`${API_URL}/color`);
